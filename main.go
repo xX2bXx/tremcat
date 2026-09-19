b58324d83b1b56f5c8e7ee4e631b71975276c93e
xX2bXx <188763131+xX2bXx@users.noreply.github.com>
2026-09-19 12:26:39 +0300

    feat: initial tremcat implementation

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"math/rand/v2"
+	"os"
+	"os/exec"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// check if chafa is installed on a host machine
+	if _, err := exec.LookPath("chafa"); err != nil {
+		log.Fatalf("chafa not found: %v", err)
+	}
+	
+	r := gin.Default()
+
+	// get all the filenames in pics/
+	entries, err := os.ReadDir("pics")
+	if err != nil {
+	    log.Fatal(err)
+	}
+
+	files := make([]string, 0, len(entries))
+	
+	for _, entry := range entries {
+	    if !entry.IsDir() {
+	        files = append(files, "pics/" + entry.Name())
+	    }
+	}
+	
+	r.GET("/", func(c *gin.Context) {
+		// get user's terminal size
+		w := c.DefaultQuery("w", "80")
+		h := c.DefaultQuery("h", "45")
+
+		randPic := files[rand.IntN(len(files))]
+		out, err := exec.Command("chafa", "--size", w+"x"+h, randPic).Output()
+		if err != nil {
+			log.Printf("chafa failed: %v", err)
+			c.String(500, "failed to render a cat")
+			return
+		}
+		
+		c.Data(200, "text/plain; charset=utf-8", out)
+	})
+	r.Run(":8080")
+}
